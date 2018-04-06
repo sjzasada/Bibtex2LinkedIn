54fdd2759caf9f8a0b464828d842205b5686da74
Stefan Zasada <sjzasada@gmail.com>
2018-04-06 10:45:09 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Bibtex2LinkedIn
-Import a Bibtex database to your LinkedIn publications list 
+
+A Python 3 script to convert a Bibtex database to the [LinkedIn journal publications](https://developer.linkedin.com/docs/ref/v2/profile/publication) format for import using v2 of the [LinkedIn REST API](https://developer.linkedin.com/docs/guide/v2/people/profile-edit-api/publications).
+
+## Usage
+
+`pythion3 convert.py -f <bibtexfile> [-n <surname>] [-i <linkedin id>] [-t] [-j]`
+
+If `surname` is specified, occurrences of surname will be replaced by LinkedIn ID in the authors list.
+
+`-t` converts article titles to titlecase
+
+`-j` converts journal names to titlecase
+
+Journals are added to the publisher field on LinkedIn.
--- convert.py
+++ convert.py
@@ -1,48 +1,123 @@
+import getopt, sys
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 from titlecase import titlecase
 import json
 
-with open('citations.bib') as bibtex_file:
-    parser = BibTexParser()
-    parser.customization = convert_to_unicode
+def parseBibentry(entry, myName, myId, titcase, jorcase):
+    entry = bibtexparser.customization.author(entry)
 
-    bib_database = bibtexparser.load(bibtex_file, parser=parser)
+    title = entry['title']
+    if titcase:
+        title = titlecase(title)
 
-    for i, entry in enumerate(bib_database.entries):
-        print(i);
+    journal = ""
+    if 'journal' in entry:
+        journal = entry['journal']
+    elif 'booktitle' in entry:
+        journal = entry['booktitle']
+    elif 'school' in entry:
+        journal = entry['school']
 
-        entry = bibtexparser.customization.author(entry)
-
-        title = titlecase(entry['title'])
-
-        journal = ""
-        if 'journal' in entry:
-            journal = entry['journal']
-        elif 'booktitle' in entry:
-            journal = entry['booktitle']
-        elif 'school' in entry:
-            journal = entry['school']
-
+    if jorcase:
         journal = titlecase(journal)
-        year = entry['year']
 
-        pub = {}
-        pub['date'] = {'year': year}
+    year = entry['year']
 
-        pub['name'] = {'localized': {'en_US': title}, 'preferredLocale': {'country': 'US', 'language': 'en'}}
-        pub['publisher'] = {'localized': {'en_US': journal}, 'preferredLocale': {'country': 'US', 'language': 'en'}}
+    pub = {}
+    pub['date'] = {'year': year}
 
-        if 'url' in entry:
-            pub['url'] = entry['url']
+    pub['name'] = {'localized': {'en_US': title}, 'preferredLocale': {'country': 'US', 'language': 'en'}}
+    pub['publisher'] = {'localized': {'en_US': journal}, 'preferredLocale': {'country': 'US', 'language': 'en'}}
 
-        names = bibtexparser.customization.getnames(entry['author'])
+    if 'url' in entry:
+        pub['url'] = entry['url']
 
-        for j, name in enumerate(names):
-            nom = bibtexparser.customization.splitname(name, strict_mode=False)
+    names = bibtexparser.customization.getnames(entry['author'])
 
-            print(nom)
+    people = []
+    for j, name in enumerate(names):
+        nom = bibtexparser.customization.splitname(name, strict_mode=False)
 
-        json_data = json.dumps(pub)
-        print(json_data)
+        components = nom['first']
+        components.extend(nom['von'])
+        components.extend(nom['last'])
+        components.extend(nom['jr'])
+
+        fullname = " ".join(components)
+
+        if myName and myName in nom['last']:
+            people.append({'memberId': 'urn:li:person:'+myId})
+
+        else:
+            people.append({'name': fullname})
+
+        #LinkedIn limit of 10 authors
+        if j > 9:
+            break
+
+    pub['authors'] = people
+    return json.dumps(pub, indent=4, sort_keys=True);
+
+def usage():
+    print("usage: convert.py -f <bibtexfile> [-n <surname>] [-i <linkedin id>] [-t] [-j]\n")
+    print("If surname is specified, occurrences of surname will be replaced by LinkedIn ID in the authors list.")
+    print("-t converts article titles to titlecase, and -j converts journal names to titlecase\n")
+    return;
+
+def main():
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "tjn:i:f:", ["name=", "id=", "file="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    titcase=False
+    jorcase=False
+    name = ""
+    id = ""
+
+    for o, a in opts:
+        if o == "-t":
+            titcase = True
+        elif o == "-j":
+            jorcase = True
+        elif o in ("-f", "--file"):
+            filename = a
+        elif o in ("-n", "--name"):
+            name = a
+        elif o in ("-i", "--id"):
+            id = a
+        else:
+            assert False, "unhandled option"
+
+    #check for sensible arguments
+    if 'filename' not in locals():
+        print('Filename not set')
+        usage()
+        sys.exit(3)
+
+    if not id:
+        if name:
+            print("Name set but ID not specified")
+            usage()
+            sys.exit(4)
+
+    with open(filename) as bibtex_file:
+        parser = BibTexParser()
+        parser.customization = convert_to_unicode
+        bib_database = bibtexparser.load(bibtex_file, parser=parser)
+
+        for i, entry in enumerate(bib_database.entries):
+            print('Entry {}'.format(i));
+
+            print(parseBibentry(entry, name, id, titcase, jorcase))
+
+    return;
+
+if __name__ == '__main__':
+    main()
